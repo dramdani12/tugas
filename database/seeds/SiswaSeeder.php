75d076a12322b8b417bbb61fa22d526837194a6e
dramdani12 <151610190@smkassalaambandung.sch.id>
2017-07-27 12:02:04 +0700

    all

--- database/seeds/SiswaSeeder.php
+++ database/seeds/SiswaSeeder.php
@@ -14,6 +14,8 @@ class SiswaSeeder extends Seeder
         $S=[
         ['nama'=>'nadiyah','tanggal_lahir'=>'2000-12-04','tempat'=>'sayuran'],
         ['nama'=>'dani','tanggal_lahir'=>'1998-12-25','tempat'=>'citepus'],
+        ['nama'=>'melda','tanggal_lahir'=>'2000-07-02','tempat'=>'jawa'],
+        ['nama'=>'iswanto','tanggal_lahir'=>'2000-05-29','tempat'=>'bandung'],
         ['nama'=>'nisa','tanggal_lahir'=>'2000-02-28','tempat'=>'hiumacan']
         ];
         DB::table('Siswa')->insert($S);
